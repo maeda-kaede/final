2e3798ec5663304638559f5f4db5889f097d0814
maeda-kaede <148741408+maeda-kaede@users.noreply.github.com>
2024-01-16 11:43:21 +0900

    Delete final directory

--- final/db-connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    const server = "mysql220.phy.lolipop.lan";
-    const dbname = "LAA1517458-final";
-    const user = "LAA1517458";
-    const pass = "Pass0506";
-
-    $connect = 'mysql:host='. server . ';dbname='. dbname . ';charset=utf8';
-?>
